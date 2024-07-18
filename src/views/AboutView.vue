a340cf3e18854b77eb75b1cfdb733169503521e9
Arpit Tripathi <arpit04tripathi@gmail.com>
2024-07-18 20:40:46 +0100

    update view pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,17 +4,9 @@ import TestBootstrap from '@/components/TestBootstrap.vue';
 
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>ABOUT PAGE </h1>
   </div>
   <TestBootstrap/>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+
